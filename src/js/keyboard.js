962b19e13559f84a1f229a98ef214657edde5f15
xokryzx <xokryzx@gmail.com>
2023-09-18 04:01:05 +0900

    ✨ Add a virtual keyboard

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,6 +1,9 @@
 export class Keyboard {
   #switch;
   #fontSelect;
+  #inputContainer;
+  #input;
+  #keyboardContainer;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -9,6 +12,9 @@ export class Keyboard {
   #assignElement() {
     this.#switch = document.querySelector(".switch__input");
     this.#fontSelect = document.querySelector("#font");
+    this.#inputContainer = document.querySelector(".input-container");
+    this.#input = document.querySelector(".input");
+    this.#keyboardContainer = document.querySelector(".keyboard-container");
   }
 
   #addEvent() {
@@ -20,6 +26,23 @@ export class Keyboard {
     });
     this.#fontSelect.addEventListener("change", (event) => {
       document.documentElement.style.fontFamily = event.target.value;
+    });
+    document.addEventListener("keydown", (event) => {
+      this.#inputContainer.classList.toggle(
+        "error",
+        /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key),
+      );
+      this.#keyboardContainer
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.add("active");
+    });
+    document.addEventListener("keyup", (event) => {
+      this.#keyboardContainer
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.remove("active");
+    });
+    this.#input.addEventListener("input", () => {
+      this.#input.value = this.#input.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
     });
   }
 }
