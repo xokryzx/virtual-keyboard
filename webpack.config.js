2170bd51aa459e6b41fb019115ff3f5f5eebf36c
xokryzx <xokryzx@gmail.com>
2023-09-18 03:44:50 +0900

    🎉 Initialize the repository and add a basic project configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    open: true,
+    watchFiles: "index.html",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: "body",
+      favicon: "./favicon.ico",
+    }),
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserWebpackPlugin(), new CssMinimizerWebpackPlugin()],
+  },
+};
